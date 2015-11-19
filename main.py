029685a76c7f7b171dd86eef64cbcddcde86b6bb
Silas Barta <silas.barta@hiqlabs.com>
2015-11-19 13:41:50 -0800

    Set up initial functions.

---
+++ main.py
@@ -0,0 +1,16 @@
+def sumof2(x, y):
+    return x + y
+
+def fib_recursive(n):
+    if n == 0:
+        return 1
+    return n * fib_recursive(n - 1)
+
+def fib_iterative(n):
+    out = 1
+    for x in xrange(1, n + 1):
+        out *= x
+    return out
+
+if __name__ == "__main__":
+    print "Hello World!"

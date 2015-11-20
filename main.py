9d36781714944223ca59e1372cdf5eab6ee83803
Silas Barta <silas.barta@hiqlabs.com>
2015-11-20 09:26:31 -0800

    Comment on recursive factorial

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ def sumof2(x, y):
     return x + y
 
 def fac_recursive(n):
+    """You should use this one because it's more elegant."
+    """
     if n == 0:
         return 1
     return n * fib_recursive(n - 1)
 
 def fac_iterative(n):
+    """You shouldn't use this one because it's less elegant."
+    """
     out = 1
     for x in xrange(1, n + 1):
         out *= x

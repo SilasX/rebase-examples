0b9a7654f3138bd2a7d0df0a14c58d361a1d5e84
Silas Barta <silas.barta@hiqlabs.com>
2015-11-20 09:44:40 -0800

    Comment on functions.

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ def sumof2(x, y):
     return x + y
 
 def fac_recursive(n):
+    "This is better because it makes use of tail-call magic juice."
     if n == 0:
         return 1
     return n * fib_recursive(n - 1)
 
 def fac_iterative(n):
+    "Don't bother"
     out = 1
     for x in xrange(1, n + 1):
         out *= x
